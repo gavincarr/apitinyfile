780e614831e796d6d48d58719f648489bfb4a5dc
Gavin Carr <gavin@openfusion.net>
2023-06-07 16:22:47 +1000

    Add readonly and writeonly unit tests.

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 
@@ -159,6 +160,124 @@ func TestAuth(t *testing.T) {
 	r.ServeHTTP(w, req)
 	if w.Code != 404 {
 		t.Errorf("GET returned status %d, expected 404", w.Code)
+		return
+	}
+}
+
+func TestReadonly(t *testing.T) {
+	t.Parallel()
+
+	dir := t.TempDir()
+	env := Env{
+		Read:      true,
+		Write:     false,
+		Delete:    false,
+		Passwd:    "testdata/htpasswd",
+		Directory: dir,
+	}
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+	env.setupRouter(r)
+	data := "different test data\n"
+	b := strings.NewReader(data)
+
+	// Auth GET - ok, expect a real 404
+	req, _ := http.NewRequest("GET", "/foo", nil)
+	req.SetBasicAuth(testuser, testpass)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 404 {
+		t.Errorf("GET returned status %d, expected 404", w.Code)
+		return
+	}
+
+	// Auth PUT - not ok, expect a no-route 404
+	req, _ = http.NewRequest("PUT", "/foo", b)
+	req.SetBasicAuth(testuser, testpass)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 404 {
+		t.Errorf("PUT returned status %d, expected 404", w.Code)
+		return
+	}
+
+	// Auth DELETE - not ok, expect a no-route 404
+	req, _ = http.NewRequest("DELETE", "/foo", b)
+	req.SetBasicAuth(testuser, testpass)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 404 {
+		t.Errorf("DELETE returned status %d, expected 404", w.Code)
+		return
+	}
+
+	// Write a test file to dir manually
+	err := os.WriteFile(dir+"/foo", []byte(data), 0644)
+	if err != nil {
+		t.Errorf("error writing test file manually: %s", err.Error())
+		return
+	}
+
+	// Auth GET - ok, expect content
+	req, _ = http.NewRequest("GET", "/foo", nil)
+	req.SetBasicAuth(testuser, testpass)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Errorf("GET returned status %d, expected 200", w.Code)
+		return
+	}
+	got := w.Body.String()
+	if got != data {
+		t.Errorf("GET returned %q, expected %q", got, data)
+		return
+	}
+}
+
+func TestWriteonly(t *testing.T) {
+	t.Parallel()
+
+	dir := t.TempDir()
+	env := Env{
+		Read:      false,
+		Write:     true,
+		Delete:    true,
+		Passwd:    "testdata/htpasswd",
+		Directory: dir,
+	}
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+	env.setupRouter(r)
+	data := "different test data\n"
+	b := strings.NewReader(data)
+
+	// Auth PUT - ok
+	req, _ := http.NewRequest("PUT", "/foo", b)
+	req.SetBasicAuth(testuser, testpass)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 204 {
+		t.Errorf("PUT returned status %d, expected 204", w.Code)
+		return
+	}
+
+	// Auth GET - not ok, expect a no-route 404 (even though PUT was successful)
+	req, _ = http.NewRequest("GET", "/foo", nil)
+	req.SetBasicAuth(testuser, testpass)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 404 {
+		t.Errorf("GET returned status %d, expected 404", w.Code)
+		return
+	}
+
+	// Auth DELETE - ok
+	req, _ = http.NewRequest("DELETE", "/foo", b)
+	req.SetBasicAuth(testuser, testpass)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 204 {
+		t.Errorf("DELETE returned status %d, expected 204", w.Code)
 		return
 	}
 }
